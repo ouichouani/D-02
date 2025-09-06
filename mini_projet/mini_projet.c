d8ba673c37909124eacb83ca25a4f02a3616fb9c
ouichouani <ouichouaniabdlhakim@gmal.com>
2025-09-06 16:09:14 +0100

    put the mini project into its folder

--- mini_projet/mini_projet.c
+++ mini_projet/mini_projet.c
@@ -1,68 +1,242 @@
-
-// Système de Gestion de Stock dans une Librairie
-
-// Ajouter un livre au stock.
-// Afficher tous les livres disponibles.
-// Rechercher un livre par son titre.
-// Mettre à jour la quantité d'un livre.
-// Supprimer un livre du stock.
-// Afficher le nombre total de livres en stock.
-
-// 1.built tables
+#include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
 
-int main()
-{
+int main () {
 
-    char titles[255][255];
-    char author[255][255];
-    int price[255];
-    int quantity[255];
-    int chois;
-    int counter = 0;
-    char trach[255];
+    //DECLARATION DE TABLEAU QUI SE COMPOSE DE 4 TABLEAU SEPARER
 
-    printf("CHOUSE A NUMBER BETWEEN 1 AND 4 \n1.add a book\n2.show all books\n3.search on book whit it\'s title\n4.update the quantity of a giving book\n5.delete a book\nshow the total of all books\n");
-    printf("-------------------------------------------\n  -your chois : ");
+    char auteur[255][255] , titre[255][255] ;
+    int  prix[255] , quantite[255] , choix  ;
+    bool exit = false;
+    int books_counter = 0;
+    // int c;
+    char titre_de_recherch[255] ;
 
-    scanf("%d", &chois);
 
-    switch (chois)
-    {
-    case 1:
-        // If you have used scanf or similar input before
-        int c;
-        while ((c = getchar()) != '\n' && c != EOF); // clear buffer
-        
-        printf("    -add a book\n");
-        printf("give the title of the new bok: ");
-        fgets(titles[counter], sizeof(titles[counter]), stdin);
-        printf("\n%s", titles[counter]);
 
-        // scanf("%s", &trach );
 
-        // printf("give the author of the new bok: ");
-        // fgets( author[counter] , sizeof(author[counter]), stdin );
-        // scanf("%s", &trach );
+    //MENU
+    while(!exit){
+    
+        printf("+---------------------------------------------------------------+\n") ;
+        printf("|1.ajouter un livre                                             |\n") ;
+        printf("|2.modifier la quantite d'un livre                              |\n") ;
+        printf("|3.supprimer un livre                                           |\n") ;
+        printf("|4.afficher le stock                                            |\n") ;
+        printf("|5.recherch par un livre                                        |\n") ;
+        printf("|6.aficher le nembre totale des livres                          |\n") ;
+        printf("|7.arreter le programme                                         |\n") ;
+        printf("+---------------------------------------------------------------+\n\n") ;
 
-        // printf("1. %s \n2. %s \n3.t : %s",titles[counter] , author[counter], trach );
+        printf("ton choix : ") ;
+        scanf("%d" , &choix ) ;
 
-        break;
-    case 2:
-        printf("you chouse 2-------------------------------------------\n");
-        break;
-    case 3:
-        printf("you chouse 3-------------------------------------------\n");
-        break;
-    case 4:
-        printf("you chouse 4-------------------------------------------\n");
-        break;
-    case 5:
-        scanf("you chouse 5-------------------------------------------\n");
-        break;
+        system("cls");
+        switch(choix){
+            case 1 :
+                printf("+---------------------------------------------------------------+\n");
+                printf("|1.ajouter un livre                                             |\n");
+                printf("+---------------------------------------------------------------+\n\n"); 
 
-    default:
-        scanf("you chouse 5-------------------------------------------\n");
-        break;
+                getchar() ;
+
+                printf("siser le titre ici : ") ;
+                fgets(titre[books_counter] , sizeof(titre[books_counter]) , stdin) ;
+                titre[books_counter][strlen(titre[books_counter]) -1] = '\0' ;
+                
+                printf("siser le nom d'auteur ici : ") ;
+                fgets(auteur[books_counter] , sizeof(auteur[books_counter]) , stdin) ;
+                auteur[books_counter][strlen(auteur[books_counter]) -1] = '\0' ;
+                
+                printf("siser le prix de livre ici : ") ;
+                scanf("%d" , &prix[books_counter]);
+
+                printf("siser la quantite ici : ") ;
+                scanf("%d" , &quantite[books_counter]);
+
+                books_counter ++ ;
+
+                printf("+---------------------------------------------------------------+\n");
+                printf("|livre a ete ajouter avec success                               |\n");
+                printf("+---------------------------------------------------------------+\n\n"); 
+
+                break ;
+
+            case 2 :
+
+                printf("+---------------------------------------------------------------+\n");
+                printf("|2.modifier la quantite d'un livre                              |\n");
+                printf("+---------------------------------------------------------------+\n\n"); 
+
+                
+
+                getchar() ;
+
+                printf("donne le titre de livre qui vous voulez mettre a joure : ") ;
+                fgets(titre_de_recherch , sizeof(titre_de_recherch) , stdin) ;
+                titre_de_recherch[strlen(titre_de_recherch) - 1] = '\0' ;
+
+                for (int i = 0 ; i < books_counter ; i++){
+
+                    if (!strcmp(titre_de_recherch , titre[i]) ){
+                        printf("donne la nouvelle quantite de ce livre : ") ;
+                        scanf("%d" , &quantite[i]);
+
+                        printf("+---------------------------------------------------------------+\n");
+                        printf("|la quantite de livre a ete mise a jour avec success            |\n");
+                        printf("+---------------------------------------------------------------+\n\n"); 
+
+                        break ;
+                    }else if(i + 1 == books_counter ){
+                        printf("%d" , strcmp(titre_de_recherch , titre[i]));
+                        printf("+---------------------------------------------------------------+\n");
+                        printf("|le livre n'est pas trouver                                     |\n");
+                        printf("+---------------------------------------------------------------+\n\n"); 
+                        break ;
+                    }
+                }
+
+                getchar();
+
+                break ;
+
+            case 3 :
+                if(!books_counter) break ;
+
+                printf("+---------------------------------------------------------------+\n");
+                printf("|3.supprimer un livre                                           |\n");
+                printf("+---------------------------------------------------------------+\n\n"); 
+                
+                getchar() ; // remove any thing still in buffer
+
+                printf("donne le titre de livre qui vous voulez supprimer : ") ;
+                fgets(titre_de_recherch , sizeof(titre_de_recherch) , stdin) ;
+                titre_de_recherch[strlen(titre_de_recherch) - 1] = '\0' ;
+
+
+                for (int i = 0 ; i < books_counter ; i++){
+                    if (!strcmp(titre_de_recherch, titre[i]) ){
+                        printf("la supprition en coure ... \n") ;
+                        for(int j = i ; j < books_counter ; j++  ){
+                            
+                                strcpy(auteur[j] ,auteur[j+1] ) ;
+                                strcpy(titre[j] , titre[j+1]) ;
+                                prix[j] = prix[j+1] ;
+                                quantite[j] = quantite[j+1] ;
+                        }
+                        books_counter -- ;
+                        printf("+---------------------------------------------------------------+\n");
+                        printf("|le livre a ete supprimer avec success                          |\n");
+                        printf("+---------------------------------------------------------------+\n\n"); 
+                        break ;
+
+                    }else if(i + 1 == books_counter ){
+                        printf("+---------------------------------------------------------------+\n");
+                        printf("|le livre n'est pas trouver                                     |\n");
+                        printf("+---------------------------------------------------------------+\n\n"); 
+                        break ;
+                    }
+                }
+                getchar() ;
+                break ;
+            case 4 :
+
+                if(books_counter <= 0) {
+                    printf("+---------------------------------------------------------------+\n");
+                    printf("|le stock est vide                                              |\n");
+                    printf("+---------------------------------------------------------------+\n\n"); 
+
+                    getchar() ;
+                    getchar() ;
+
+                    break ;
+                }
+                printf("+---------------------------------------------------------------+\n");
+                printf("|4.afficher le stock                                            |\n");
+                printf("+---------------------------------------------------------------+\n\n"); 
+                
+                printf("+---------------------------------------------------------------+\n");  
+
+                for( int i = 0 ; i < books_counter ; i++ ){
+                    printf("|titre             : %s\n" , titre[i]) ;
+                    printf("|auteur            : %s\n" , auteur[i]) ;
+                    printf("|quantite restent  : %d\n" , quantite[i]) ;
+                    printf("|prix de livre     : %d\n" , prix[i]) ;
+                    printf("+---------------------------------------------------------------+\n") ;
+                }
+
+                getchar() ;
+                getchar() ;
+                break ;
+
+            case 5 :
+                printf("+---------------------------------------------------------------+\n");
+                printf("|5.recherch par un livre                                        |\n") ;
+                printf("+---------------------------------------------------------------+\n\n"); 
+                
+                getchar() ;
+
+                printf("donne le titre de livre qui vous voulez trouver a joure : ") ;
+                fgets(titre_de_recherch , sizeof(titre_de_recherch) , stdin) ;
+                titre_de_recherch[strlen(titre_de_recherch) - 1] = '\0' ;
+
+                    
+                for (int i = 0 ; i < books_counter ; i++){
+
+                    if (!strcmp(titre_de_recherch , titre[i]) ){
+
+                        printf("+---------------------------------------------------------------+\n") ;
+                        printf("|titre             : %s\n" , titre[i]) ;
+                        printf("|auteur            : %s\n" , auteur[i]) ;
+                        printf("|quantite restent  : %d\n" , quantite[i]) ;
+                        printf("|prix de livre     : %d\n" , prix[i]) ;
+                        printf("+---------------------------------------------------------------+\n\n") ;
+                        break ;
+                        
+                    }else if(i + 1 == books_counter ){
+
+                        printf("+---------------------------------------------------------------+\n") ;
+                        printf("|le livre n'est pas trouver                                     |\n");
+                        printf("+---------------------------------------------------------------+\n") ;
+                        break ;
+                    }
+                }
+
+                getchar() ;
+
+                break ;
+                
+            case 6 :
+                int totale_book = 0 ;
+                for(int i = 0 ; i < books_counter ; i++){
+                    totale_book += quantite[i] ;
+                    
+                }
+                printf("+---------------------------------------------------------------+\n") ;
+                printf("|le nembre totale de tous les livres restant est : %d\n" , totale_book );
+                printf("+---------------------------------------------------------------+\n") ;
+
+                getchar();
+                getchar();
+
+                break ;
+            
+            case 7 :
+                exit = 1 ;
+                break ;
+                    
+            default :
+                    
+                    printf("+---------------------------------------------------------------+\n") ;
+                    printf("|choisir un nombrre entre 1 et 6                                |\n" );
+                    printf("+---------------------------------------------------------------+\n") ;
+
+                    getchar() ;
+                    getchar() ;
+                    break;
+        }
+        system("cls");
     }
 }
